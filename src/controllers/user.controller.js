4a90731fa6771f235cf82361b571e6ac579c8b46
frozenopertor <adityagupta11112002@gmail.com>
2024-02-06 00:34:27 +0530

    bug fixed in refreshAccessToken in user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -177,7 +177,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized Request");
   }
   try {
